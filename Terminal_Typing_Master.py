2291be91678a7449cefe23e754fe7fbf21f97a9e
RISHABH KUMAR SONKAR <official.rishabh2028@gmail.com>
2024-01-16 21:30:25 +0530

    added new file

--- Terminal_Typing_Master.py
+++ Terminal_Typing_Master.py
@@ -21,3 +21,14 @@ def showing_leaderboard():
     for entry in leaderboard:
         print(f"{entry['username']}\t\t{entry['wpm']}")
     print("===================")
+
+# Function to load words from a JSON file based on the selected category
+def loading_words_from_json(category):
+    file_path = f"{category.lower()}_words.json"
+    try:
+        with open(file_path, "r", encoding="utf-8") as file:
+            words_data = json.load(file)
+            return words_data["words"]
+    except (FileNotFoundError, json.JSONDecodeError) as e:
+        print(f"Error loading words: {e}")
+        return []
